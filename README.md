866e1baed865555e009fb6277b54251c6fd7ffe0
sahilsuri008 <sahilsuri008@example.com>
2019-08-05 15:53:48 +0000

    wrote script hello.bash in ch01 dir

---
+++ README.md
@@ -0,0 +1,1 @@
+This is a collection of bash shell scripts I wrote while studying from a video course on the subject.
---
+++ ch01/hello.bash
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+echo "Hello World! This is my first bash script!"
+echo -n "The script is executed as user: "
+whoami
+echo -n "The script is executing from the directory: "
+pwd
+
+exit 0
