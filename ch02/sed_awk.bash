c8ec7e15504f548e553f18ba2acb91a27280a213
sahilsuri008 <sahilsuri008@example.com>
2019-08-11 15:44:08 +0000

    added scripts to demo sed/awk

---
+++ ch02/sed_awk.bash
@@ -0,0 +1,54 @@
+#!/bin/bash
+
+GB_CSV="testdata/garbage.csv"
+EM_CSV="testdata/employees.csv"
+
+set IFS=,
+set oldIFS = $IFS
+readarray -t ARR < ${GB_CSV}
+
+ARRY_ELEM=${#ARR[@]}
+echo "We have ${ARRY_ELEM} rows in ${GB_CSV}"
+
+INC=0
+for i in "${ARR[@]}"
+do
+   :
+  ARR[$INC]=$(echo $i | sed 's/ //g')
+  echo "${ARR[$INC]}"
+  INC=$[$INC+1]
+done
+
+INC=0
+for i in "${ARR[@]}"
+do
+   :
+  ARR[$INC]=$(echo $i | sed 's/.$//' | sed -e 's/.*/\U&/' )
+  echo "${ARR[$INC]}"
+  INC=$[$INC+1]
+done
+
+set IFS=,
+set oldIFS = $IFS
+readarray -t ARR < ${EM_CSV}
+
+INC=0
+for i in "${ARR[@]}"
+do
+   :
+  ARR[$INC]=$(sed -e 's/^/#/' <<< $i )
+  echo "${ARR[$INC]}"
+  INC=$[$INC+1]
+done
+
+
+sed -i 's/Bob/Robert/' ${EM_CSV}
+sed -i 's/^/#/' ${EM_CSV}
+cat ${EM_CSV}
+
+#Remove column number 5#
+#gsub(REGEXP, REPLACEMENT, TARGET)'
+awk 'BEGIN { FS=","; OFS="," } {$5="";gsub(",+",",",$0)}1' OFS=, ${EM_CSV}
+
+#Print only first field of the first column#
+awk 'BEGIN {FS=","} NR==1 {print $1}' ${EM_CSV}
